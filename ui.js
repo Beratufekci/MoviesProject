971e9eca83fcb62372540c8b26d563d089153c6e
DESKTOP-0FBHRAR\DELL <berat.tufekci@kafein.com.tr>
2021-02-11 17:28:23 +0300

    Loading All Movies from Storage

--- ui.js
+++ ui.js
@@ -6,17 +6,13 @@ UI.prototype.addFilmToUI = function(newFilm){
    
 const filmList = document.querySelector("#films");
 filmList.innerHTML +=`
-
        <tr>
            <td><img src="${newFilm.url}" class="img-fluid img-thumbnail"></td>
            <td>${newFilm.title}</td>
            <td>${newFilm.director}</td>
            <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
        </tr> -->
-  
-
-
-`
+`;
 }
 
 UI.prototype.clearInputs = function(element1,element2,element3){
@@ -38,4 +34,20 @@ UI.prototype.displayMessages = function(message,type){
     setTimeout(function(){
         div.remove();
     },1000);
+}
+
+UI.prototype.loadAllFilms = function(films){
+
+    const filmList = document.querySelector("#films");
+
+    films.forEach(function(film){
+        filmList.innerHTML += `
+        <tr>
+            <td><img src="${film.url}" class="img-fluid img-thumbnail"></td>
+            <td>${film.title}</td>
+            <td>${film.director}</td>
+            <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+        </tr> -->
+ `;
+    });
 }
